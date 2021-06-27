f9a6c4e75b8c327748813306b311e2a7c71d23ea
Vitalik Baiduk <vitalilbaiduj@icloud.com>
2021-06-27 16:17:26 +0300

    commit: did the last task

--- script.js
+++ script.js
@@ -1,16 +1,86 @@
 "use sctict";
 
-// let buttom = document.querySelector(".button");
-// let input = document.querySelector(".entryField")
-// buttom.addEventListener("click", function(event) {
-//     event.defaultPrevented;
-//     if (input.value == "google") {
-//         alert("Yandex круче.Но это не точно");
-//     } else {
-//         alert(input.value);
-//     }
-// });
+let button = document.querySelector(".button");
+let input = document.querySelector(".entryField");
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (input.value == "google") {
+        alert("Yandex круче.Но это не точно");
+    } else {
+        alert(input.value);
+    }
+});
 
+let arrObj = [];
+arrObj[0] = {
+    name: "Alex",
+    age: 12
+};
+arrObj[1] = {
+    name: "John",
+    age: 13
+};
+arrObj[2] = {
+    name: "Jack",
+    age: 14
+};
+arrObj[3] = {
+    name: "Bob",
+    age: 15
+};
+
+button.addEventListener("click", function() {
+    alert(arrObj[0].name);
+});
+
+function superSum(firstNum, secondNum) {
+    alert(firstNum + secondNum);
+}
+superSum(3, 7);
+
+let unsortedArr = [7, 3, 5, 9, 2, 6, 4, 8];
+let maxNum = 0;
+let minNum = unsortedArr[0];
+
+function searchMinOrMaxNum() {
+    for (let i = 0; i <= unsortedArr.length; i++) {
+        if (maxNum < unsortedArr[i]) {
+            maxNum = unsortedArr[i];
+        } else if (minNum > unsortedArr[i]) {
+            minNum = unsortedArr[i];
+        }
+    }
+    alert(maxNum);
+    alert(minNum);
+}
+searchMinOrMaxNum();
+
+
+let a = "XXX";
+let b = "YYY";
+[b, a] = [a, b];
+alert(a);
+alert(b);
+
+//Оформить алгоритм поиска максимального числа в массиве как функцию findMax (чтобы можно было передавать в неё любой массив чисел, а на выходе она возвращала нам максимальное число из массива)
+let maxNum2 = 0;
+let arr2 = [2, 6, 4, 1, 4, 5, 8, 42, 4, 6, 7];
+
+function searchMaxNum(arr) {
+    for (let i = 0; i <= arr.length; i++) {
+        if (maxNum2 < arr[i]) {
+            maxNum2 = arr[i];
+        }
+    }
+    alert(maxNum2);
+}
+searchMaxNum(arr2);
+
+//показать alert с результатом поиска не сразу, а через 3 секунды после нажатия (всё то же самое, как и работало, в частности с “яндекс круче всех”, но с задержкой в 3 секунды)
+// нужно закомментировать код 3-34 строки для работы с этим кодом 
+
+// let button = document.querySelector(".button");
+// let input = document.querySelector(".entryField");
 // let arrObj = [];
 // arrObj[0] = {
 //     name: "Alex",
@@ -29,112 +99,14 @@
 //     age: 15
 // };
 
-// buttom.addEventListener("click", function() {
-//     alert(arrObj[0].name);
-// });
-
-// function superSum(firstNum, secondNum) {
-//     alert(firstNum + secondNum);
-// }
-// superSum(3, 7);
-
-// let unsortedArr = [7, 3, 5, 9, 3, 6, 4, 8];
-// let maxNum = 0;
-// let minNum = unsortedArr[0];
-
-// function searchMinOrMaxNum() {
-//     for (let i = 0; i <= unsortedArr.length; i++) {
-//         if (maxNum < unsortedArr[i]) {
-//             maxNum = unsortedArr[i];
-//         } else if (minNum > unsortedArr[i]) {
-//             minNum = unsortedArr[i];
+// button.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     setTimeout(() => {
+//         if (input.value == "google") {
+//             alert("Yandex круче.Но это не точно");
+//         } else {
+//             alert(input.value);
 //         }
-//     }
-//     alert(maxNum);
-//     alert(minNum);
-// }
-// searchMinOrMaxNum();
-
-
-// let a = "XXX";
-// let b = "YYY";
-// [b, a] = [a, b];
-// alert(a);
-// alert(b);
-
-
-// let maxNum2 = 0;
-
-// function searchMaxNum(arr) {
-//     for (let i = 0; i <= arr.length; i++) {
-//         if (maxNum2 < arr[i]) {
-//             maxNum2 = arr[i];
-//         }
-//     }
-//     alert(maxNum2);
-// }
-
-
-let buttom = document.querySelector(".button");
-let input = document.querySelector(".entryField");
-
-// function showM() {
-//     setTimeout(function(event) {
-//         event.defaultPrevented();
-//         // if (input.value == "google") {
-//         //     alert("Yandex круче.Но это не точно");
-//         // } else {
-//         //     alert(input.value);
-//         // }
-//         console.log(input.value);
+//         alert(arrObj[0].name);
 //     }, 3000);
-// }
-function myFunc() {
-    setTimeout(function() {
-        console.log("Hello");
-    }, 3000);
-}
-buttom.addEventListener("click", myFunc);
-const showMessage = function() {
-
-
-    let arrObj = [];
-    arrObj[0] = {
-        name: "Alex",
-        age: 12
-    };
-    arrObj[1] = {
-        name: "John",
-        age: 13
-    };
-    arrObj[2] = {
-        name: "Jack",
-        age: 14
-    };
-    arrObj[3] = {
-        name: "Bob",
-        age: 15
-    };
-
-    buttom.addEventListener("click", function() {
-        alert(arrObj[0].name);
-    });
-    console.log(input.value);
-};
-// setTimeout(function(event) {
-//     event.preventDefault();
-//     if (input.value == "google") {
-//         alert("Yandex круче.Но это не точно");
-//     } else {
-//         alert(input.value);
-//     }
-// }, 3000);
-// setTimeout(function(event) {
-//     event.defaultPrevented();
-//     // if (input.value == "google") {
-//     //     alert("Yandex круче.Но это не точно");
-//     // } else {
-//     //     alert(input.value);
-//     // }
-//     console.log(input.value);
-// }, 3000);
+// });
